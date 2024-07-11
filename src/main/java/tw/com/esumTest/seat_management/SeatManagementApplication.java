6353fdfc5c37016d96b0a496a60ba0e875c52342
YU <popoan20@gmail.com>
2024-07-11 23:04:58 +0800

    no message

---
+++ src/main/java/tw/com/esumTest/seat_management/SeatManagementApplication.java
@@ -0,0 +1,13 @@
+package tw.com.esumTest.seat_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/esumTest/seat_management/SeatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.esumTest.seat_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
